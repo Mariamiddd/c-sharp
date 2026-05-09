e600395591db58a6c110eee38b85a7f0a2cad800
Mariamiddd <mariami.tediashvili9076@hum.tsu.edu.ge>
2026-05-09 21:56:40 +0400

    adding first exercise of homework

---
+++ c-sharp/operators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_sharp.operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            //*davaleba 1 dakomentarebuli //*
+            //Console.WriteLine("enter your age");
+
+            //string input = Console.ReadLine();
+
+            //int age = int.Parse(input);
+            //string message = age >= 18 ? "გილოცავ, შენ შეგიძლია ხმისმ მიცემა!" : "სამწუხაროდ, შენ არ შეგიძლია ხმის მიცემა!";
+            //Console.WriteLine(message);
+
+//* davaleba 2 dakomentarebuli */
+
+        }
+    }
+}
