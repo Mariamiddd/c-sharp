e095e7fdb372fc8ec17cc1b4ce851657fa0acd82
Mariamiddd <mariami.tediashvili9076@hum.tsu.edu.ge>
2026-05-10 22:40:29 +0400

    meore davaleba

--- c-sharp/operators/Program.cs
+++ c-sharp/operators/Program.cs
@@ -19,8 +19,37 @@ namespace c_sharp.operators
             //string message = age >= 18 ? "გილოცავ, შენ შეგიძლია ხმისმ მიცემა!" : "სამწუხაროდ, შენ არ შეგიძლია ხმის მიცემა!";
             //Console.WriteLine(message);
 
-//* davaleba 2 dakomentarebuli */
+// davaleba 2 dakomentarebuli 
 
+          //Console.WriteLine("enter first number");
+          //  string input1 = Console.ReadLine();
+          //  // conevrting to str tonumber 
+          //  int num1 = int.Parse(input1);
+
+          //  Console.WriteLine("enter second number");
+          //  string input2 = Console.ReadLine();
+          //  // conevrting to str tonumber 
+          //  int num2 = int.Parse(input2);
+
+          //  Console.WriteLine("enter third number");
+
+          //  string input3 = Console.ReadLine();
+          //  // conevrting to str tonumber 
+          //  int num3 = int.Parse(input3);
+
+
+          //  if (num1 >= num2 && num1 >= num3)
+          //  {
+          //      Console.WriteLine("პირველი რიცხვი მაქსიმალურია წარმოდგენილ რიცხვებს შორის");
+          //  }
+          //  else if (num2 >= num1 && num2 >= num3)
+          //  {
+          //      Console.WriteLine("მე-2 რიცხვი მაქსიმალურია წარმოდგენილ რიცხვებს შორის");
+          //  }
+          //  else
+          //  {
+          //      Console.WriteLine("მე-3 რიცხვი მაქსიმალურია წარმოდგენილ რიცხვებს შორის");
+          //  }
         }
     }
 }
