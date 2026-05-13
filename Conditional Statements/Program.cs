cbd3cf2b0e8c5cafa257db6335a60622bb20d960
Mariamiddd <mariami.tediashvili9076@hum.tsu.edu.ge>
2026-05-14 01:00:10 +0400

    first exercise

---
+++ Conditional Statements/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Conditional_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string userName = "admin";
+            string password = "1234";
+
+            Console.WriteLine("enter userName");
+
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine("enter passsword");
+            string passInput = Console.ReadLine();
+
+            if (userInput == userName && passInput == password)
+            {
+                Console.WriteLine("welcome to the system");
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("invalid username or password");
+            }
+        } 
+    }
+}
