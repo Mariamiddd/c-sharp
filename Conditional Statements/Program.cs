65b84407c5a6fa901df9480ae46a1d781032a613
Mariamiddd <mariami.tediashvili9076@hum.tsu.edu.ge>
2026-05-14 17:35:11 +0400

    /

--- Conditional Statements/Program.cs
+++ Conditional Statements/Program.cs
@@ -5,27 +5,97 @@
         static void Main(string[] args)
         {
 
-            string userName = "admin";
-            string password = "1234";
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            Console.WriteLine("enter userName");
+            #region davaleba1
+            //string userName = "admin";
+            //string password = "1234";
 
-            string userInput = Console.ReadLine();
+            //Console.WriteLine("enter userName");
 
-            Console.WriteLine("enter passsword");
-            string passInput = Console.ReadLine();
+            //string userInput = Console.ReadLine();
 
-            if (userInput == userName && passInput == password)
-            {
-                Console.WriteLine("welcome to the system");
+            //Console.WriteLine("enter passsword");
+            //string passInput = Console.ReadLine();
+
+            //if (userInput == userName && passInput == password)
+            //{
+            //    Console.WriteLine("welcome to the system");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("invalid username or password");
+            //}
+
+            #endregion
+
+            #region davaleba2
+
+            //Console.WriteLine("enter first num");
+            //double firstNum = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("enter second num");
+            //double secondNum = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("enter operation");
+            //char operationInput = Convert.ToChar(Console.ReadLine());
+
+            //switch (operationInput)
+            //{
+            //    case
+            //        '+':
+            //        Console.WriteLine(firstNum + secondNum);
+            //        break;
+            //    case
+            //        '-':
+            //        Console.WriteLine(firstNum - secondNum);
+            //        break;
+            //    case
+            //        '*':
+            //        Console.WriteLine(firstNum * secondNum);
+            //        break;
+            //    case
+            //        '/':
+            //        if (secondNum != 0)
+            //        {
+            //            Console.WriteLine(firstNum / secondNum);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("cannot divide by zero");
+            //        }
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("incorrect operation");
+            //        break;
+            #endregion
+
+            #region davaleba 3
+            //Console.WriteLine("enter your age");
+            //byte age = Convert.ToByte(Console.ReadLine());
+            //switch (age)
+            //{
+            //    case >= 0 and <= 12:
+            //        Console.WriteLine("ბავშვი");
+            //        break;
+            //    case >= 13 and <= 19:
+            //        Console.WriteLine("თინეიჯერი");
+            //        break;
+            //    case >= 20 and <= 64:
+            //        Console.WriteLine("ზრდასრული");
+            //        break;
+            //    case >= 65:
+            //        Console.WriteLine("პენსიონერი");
+            //        break;
+
+            //}
+            #endregion
 
 
 
-            }
-            else
-            {
-                Console.WriteLine("invalid username or password");
-            }
-        } 
+        }
     }
 }
+
+
