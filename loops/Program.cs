4b79e25c55fd77fe0a34d24571b83c4547e1f660
Mariamiddd <mariami.tediashvili9076@hum.tsu.edu.ge>
2026-05-16 13:26:30 +0400

    loops

---
+++ loops/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel.Design;
+
+namespace loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region დავალება1
+            //Console.WriteLine("Enter number!");
+            //int num;
+            //bool isValid = int.TryParse(Console.ReadLine(), out num);
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+
+            //    int product = num * i;
+            //    Console.WriteLine(product);
+
+            //}
+            #endregion
+
+            #region დავალება2
+            //Console.WriteLine("Enter number!");
+            //int num;
+            //bool isValid = int.TryParse(Console.ReadLine(), out num);
+
+            //    for (int i = 1; i <= num; i++)
+            //    {
+            //        string spaces = new string(' ', num - i);
+
+            //        string stars = "";
+            //        for (int j = 0; j < i; j++)
+            //        {
+            //            stars += "* ";
+            //        }
+
+            //        Console.WriteLine(spaces + stars);
+            //    }
+            #endregion
+
+            #region დავალება3
+            //    Console.WriteLine("Enter number!");
+            //    int num;
+            //    bool isValid = int.TryParse(Console.ReadLine(), out num);
+
+            //    int sum = 0;
+            //    for (int i = 1; i <= num; i++)
+            //    {
+            //        if (i % 2 == 0) { 
+            //        sum = sum + i;
+
+            //    }
+            //}
+            //    Console.WriteLine(sum);
+
+            #endregion
+
+            #region დავალება4
+            //Random r = new Random();
+            
+            ////1 dan 10mde random number  
+            //int randomNumber = r.Next(1, 10);
+
+            //Console.WriteLine("Guess random number!");
+            //int guess;
+
+            //bool isValid = int.TryParse(Console.ReadLine(), out guess);
+
+            //while (!isValid || guess != randomNumber)
+            //{
+            //    Console.WriteLine("Try again!");
+            //    isValid = int.TryParse(Console.ReadLine(), out guess);
+            //}
+            //Console.WriteLine("Congratulations! You guessed the number.");
+
+            #endregion
+        }
+    }
+}
